4325cf3ac2b231ed0fe8e8d553a459bf093ebcea
Vitor-Vidotto <vitotbrbr@gmail.com>
2025-06-02 17:12:11 -0300

    css hotfix in the bg

--- src/app/theme.js
+++ src/app/theme.js
@@ -6,21 +6,19 @@ const config = {
 };
 
 const styles = {
-  global: (props) => ({
-    ":root": {
-      "--background": props.colorMode === "dark"
-        ? "linear-gradient(to bottom right, #1f2937, #000000, #1f2937)"
-        : "linear-gradient(to bottom right, #f5deb3, #dfc28e, #f5deb3)",
-      "--foreground": props.colorMode === "dark" ? "#f5f5f5" : "#333333",
-      "--navbar-background": props.colorMode === "dark" ? "#1a1a1a" : "#ededed",
-      "--navbar-text": props.colorMode === "dark" ? "#f5f5f5" : "#121212",
-      "--title-text": props.colorMode === "dark" ? "#f5f5f5" : "#121212",
+  global: {
+    html: {
+      minHeight: "100%",
     },
     body: {
-      bg: "transparent", // vai usar o --background no CSS
+      minHeight: "100%",
+      margin: 0,
+      // Usa as variáveis CSS definidas no CSS externo, sem sobrescrever
+      backgroundImage: "var(--background)",
       color: "var(--foreground)",
+      transition: "background-image 1s ease, color 1s ease",
     },
-  }),
+  },
 };
 
 const theme = extendTheme({ config, styles });
